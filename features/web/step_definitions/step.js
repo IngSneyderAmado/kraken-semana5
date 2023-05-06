1ff02a0c5acc4fecf25eb9a5c8a2c896a9c0091f
IngSneyderAmado <s.amadov@uniandes.edu.co>
2023-05-06 10:53:43 -0500

    se elimna repetidos

--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -77,21 +77,6 @@ When('I click confirmareliminaciontag', async function() {
 })
 
 
-When('I enter email {kraken-string}', async function (email) {
-    let element = await this.driver.$('#ember7');
-    return await element.setValue(email);
-});
-
-When('I enter password {kraken-string}', async function (password) {
-    let element = await this.driver.$('#ember10');
-    return await element.setValue(password);
-});
-
-When('I click next', async function() {
-    let element = await this.driver.$('#ember12');
-    return await element.click();
-});
-
 When('I click Posts menu', async function() {
     let element = await this.driver.$(".gh-nav-list-new > a[href='#/posts/']");
     return await element.click();
